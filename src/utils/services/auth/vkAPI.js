012f9f45fbe84d1aaa650ca6eb06c79fcaf3a7f0
wakeupwakeupwakeup <maslakovsana1999@gmail.com>
2023-11-10 08:58:45 +0300

    fix

--- src/utils/services/auth/vkAPI.js
+++ src/utils/services/auth/vkAPI.js
@@ -11,7 +11,7 @@ export default async function vkAPI(data) {
 
     axios.post(redirectURI, params)
         .then((response) => {
-            console.log(response)
+            console.log(response, params)
         })
         .catch((error) => console.log(error))
 }
