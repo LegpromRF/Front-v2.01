6a68626ce0fae39ad13fa9a2ce4e52a62d7143a4
wakeupwakeupwakeup <maslakovsana1999@gmail.com>
2023-11-13 17:19:36 +0300

    fixes

--- src/main.jsx
+++ src/main.jsx
@@ -29,7 +29,7 @@ const router = createBrowserRouter([
         element: <PrivateRoute><HomeProfile /></PrivateRoute>,
         children: [
             {
-                path: "/home",
+                path: "/",
                 element: <HomeProfile/>
             },
             {
