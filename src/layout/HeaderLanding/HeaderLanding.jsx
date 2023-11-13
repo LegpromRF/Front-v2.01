779a3570e0b6a78b962e57fa027817fbb7052ae4
wakeupwakeupwakeup <maslakovsana1999@gmail.com>
2023-11-13 16:46:50 +0300

    fixes

--- src/layout/HeaderLanding/HeaderLanding.jsx
+++ src/layout/HeaderLanding/HeaderLanding.jsx
@@ -1,11 +1,7 @@
 import styles from './HeaderLanding.module.scss'
 import { links } from './menu.data.js'
-
-// import Image from "next/image";
 import { useState } from 'react';
 import { useEffect } from 'react';
-
-// import Link from "next/link";
 import Logo from '@/../public/Logo/logo.png'
 import ModalAuth from "@layout/Modal/ModalAuth/ModalAuth.jsx";
 import { useDispatch, useSelector } from "react-redux";
@@ -15,16 +11,15 @@ import {Link} from "react-router-dom";
 const HeaderLanding = () => {
     const dispatch = useDispatch()
     const isShown = useSelector((state) => state.authModal.isShown)
-  const [modalActive, setModalActive] = useState(false)
-  const [activeMenu, setActiveMenu] = useState(false);
+    const [activeMenu, setActiveMenu] = useState(false);
 
-  useEffect( () => {
-    if(activeMenu) {
-      document.body.style.overflow = "hidden";
-    } else {
-      document.body.style.overflow = "visible";
-    }
-  }, [activeMenu])
+    useEffect( () => {
+        if(activeMenu) {
+          document.body.style.overflow = "hidden";
+        } else {
+          document.body.style.overflow = "visible";
+        }
+    }, [activeMenu])
 
   return ( 
     <header className={styles.header}>
--- src/layout/HeaderProfile/HeaderProfile.jsx
+++ src/layout/HeaderProfile/HeaderProfile.jsx
@@ -1,52 +1,52 @@
-import styles from './HeaderProfile.module.scss'
-import Logo from '/public/Logo/logo.png'
-import getUserName from "@/utils/services/profileData/getUserName.js";
-import {Link} from "react-router-dom";
-
-const HeaderProfile = ({active}) => {
-  // const username = getUserName()
-  // console.log(username)
-  return ( 
-    <header>
-      <div className={[styles.header, styles.header__profileContainer].join(' ')}>
-        <div className={styles.header__content}>
-          <div className={styles.header__logo}>
-            <Link to='/'>
-              <img
-                src={Logo} 
-                alt="LegpromRF"  
-                width={135} 
-                height={35}
-              />
-            </Link>
-          </div>
-          <div className={active ? [styles.header__burger, styles.header__menuActive].join(' ') : styles.header__burger}>
-            <span></span>
-          </div>
-        </div>
-        <div className={styles.home__header}>
-          {/*<div className={[styles.home__welcome, styles.home__text].join(' ')}>Добро пожаловать{username ? `, ${username} !` : '!'}</div>*/}
-          <div className={styles.home__contacts}>
-            <div className={styles.home__text}>Обращайтесь! Тел: +7-958-111-4884</div>
-            <div className={styles.home__links}>
-              <a
-                  className={""}
-                  href={"https://wa.me/+79261894737"}
-              >
-                WA
-              </a>
-              <a
-                  className={""}
-                  href={"https://t.me/LegpromRF_bot"}
-              >
-                TG
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </header>
-   );
-}
- 
-export default HeaderProfile;
+// import styles from './HeaderProfile.module.scss'
+// import Logo from '/public/Logo/logo.png'
+// import getUserName from "@/utils/services/profileData/getUserName.js";
+// import {Link} from "react-router-dom";
+//
+// const HeaderProfile = ({active}) => {
+//   // const username = getUserName()
+//   // console.log(username)
+//   return (
+//     <header>
+//       <div className={[styles.header, styles.header__profileContainer].join(' ')}>
+//         <div className={styles.header__content}>
+//           <div className={styles.header__logo}>
+//             <Link to='/'>
+//               <img
+//                 src={Logo}
+//                 alt="LegpromRF"
+//                 width={135}
+//                 height={35}
+//               />
+//             </Link>
+//           </div>
+//           <div className={active ? [styles.header__burger, styles.header__menuActive].join(' ') : styles.header__burger}>
+//             <span></span>
+//           </div>
+//         </div>
+//         <div className={styles.home__header}>
+//           {/*<div className={[styles.home__welcome, styles.home__text].join(' ')}>Добро пожаловать{username ? `, ${username} !` : '!'}</div>*/}
+//           <div className={styles.home__contacts}>
+//             <div className={styles.home__text}>Обращайтесь! Тел: +7-958-111-4884</div>
+//             <div className={styles.home__links}>
+//               <a
+//                   className={""}
+//                   href={"https://wa.me/+79261894737"}
+//               >
+//                 WA
+//               </a>
+//               <a
+//                   className={""}
+//                   href={"https://t.me/LegpromRF_bot"}
+//               >
+//                 TG
+//               </a>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </header>
+//    );
+// }
+//
+// export default HeaderProfile;
