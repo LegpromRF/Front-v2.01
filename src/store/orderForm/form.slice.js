4449aebc0bbabf2224233b62c01fe5884313fb96
wakeupwakeupwakeup <maslakovsana1999@gmail.com>
2023-11-24 06:15:10 +0300

    fixes
    Took 2 minutes

--- src/store/orderForm/form.slice.js
+++ src/store/orderForm/form.slice.js
@@ -27,7 +27,7 @@ export const formSlice = createSlice({
             state.contactsStep = true
         },
         updateFormData: (state, payload) => {
-            state.formData = {...payload}
+            state.formData = {...payload.payload}
         }
     }
 })
