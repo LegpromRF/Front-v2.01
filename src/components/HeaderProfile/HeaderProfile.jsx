429a12febee2366ae32531a92ff2f58711c71e87
wakeupwakeupwakeup <maslakovsana1999@gmail.com>
2023-11-26 07:29:51 +0300

    fixes
    Took 3 minutes

--- src/components/HeaderProfile/HeaderProfile.jsx
+++ src/components/HeaderProfile/HeaderProfile.jsx
@@ -10,7 +10,12 @@ const HeaderProfile = ({title, href, number, active}) => {
             <div className={active ? [styles.header__itemNumber, styles.header__itemNumber_active].join(' ') : styles.header__itemNumber}>
                 {
                     active?
-                        <svg fill="#ffffff" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 78.369 78.369" xml:space="preserve" stroke="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <path d="M78.049,19.015L29.458,67.606c-0.428,0.428-1.121,0.428-1.548,0L0.32,40.015c-0.427-0.426-0.427-1.119,0-1.547l6.704-6.704 c0.428-0.427,1.121-0.427,1.548,0l20.113,20.112l41.113-41.113c0.429-0.427,1.12-0.427,1.548,0l6.703,6.704 C78.477,17.894,78.477,18.586,78.049,19.015z"></path> </g> </g></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1"
+                             id="Layer_1" x="0px" y="0px" stroke="#ffffff" width="122.881px" height="89.842px" viewBox="0 0 122.881 89.842"
+                             enable-background="new 0 0 122.881 89.842" xml:space="preserve"><g><path
+                            d="M1.232,55.541c-1.533-1.388-1.652-3.756-0.265-5.289c1.388-1.534,3.756-1.652,5.29-0.265l34.053
+                            ,30.878l76.099-79.699 c1.429-1.501,3.804-1.561,5.305-0.132c1.502,1.428,1.561,3.803,0.133,5.305L
+                            43.223,88.683l-0.005-0.005 c-1.396,1.468-3.716,1.563-5.227,0.196L1.232,55.541L1.232,55.541z"/></g></svg>
                         :
                         <>
                             {number}
