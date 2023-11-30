3a9e68ee85952840bf9a66d0ccae595a53dd05b5
wakeupwakeupwakeup <maslakovsana1999@gmail.com>
2023-11-30 20:14:04 +0300

    Refactored authentication check in privateRoute.jsx to use JWT token from cookies
    Took 13 minutes

--- src/components/routing/privateRoute.jsx
+++ src/components/routing/privateRoute.jsx
@@ -1,10 +1,16 @@
-import {useSelector} from "react-redux";
 import {Navigate} from "react-router-dom";
 import PropTypes from "prop-types";
 
 function PrivateRoute({ children }) {
-    const isAuthenticated = useSelector((state) => state.auth.isAuthenticated)
-    return isAuthenticated ? children : <Navigate to={"/"}/>
+    const cookies = document.cookie
+    const cookiesArray = cookies.split(';')
+    let JWTtoken = null
+    const JWTcookie = cookiesArray.find(cookie => cookie.trim().startsWith('legpromauth'))
+    if (JWTcookie) {
+        JWTtoken = JWTcookie.split('=')[1].trim();
+    }
+
+    return JWTtoken !== null ? children : <Navigate to={"/"}/>
 }
 
 PrivateRoute.propTypes = {
