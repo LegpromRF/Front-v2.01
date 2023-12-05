12e39b985c01378388bc6e3b77d24f0e4ab5e929
davidpinkevich <davidpinkevich95@gmail.com>
2023-12-05 23:39:21 +0300

    feat: add page procurement register

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import '@styles/globals.scss'
-import {
-    createBrowserRouter,
-    RouterProvider,
-} from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "@styles/globals.scss";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Root from "./components/routing/root.jsx";
-import {Provider} from "react-redux";
-import { store } from "@store/store.js"
+import { Provider } from "react-redux";
+import { store } from "@store/store.js";
 import HomeProfile from "@/pages/profile/home.jsx";
 import NoLayout from "@/pages/nolayout.jsx";
 import VKIDtokenpage from "@/pages/VKIDtokenpage.jsx";
@@ -19,64 +16,73 @@ import TechnologyPage from "@/pages/profile/order/technology.jsx";
 import ConditionsPage from "@/pages/profile/order/conditions.jsx";
 import ContactsPage from "@/pages/profile/order/contacts.jsx";
 import SelectionPage from "@/pages/profile/selection.jsx";
+import ProcurementRegisterPage from "@/pages/profile/order/procurement.jsx";
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Root />,
-    },
-    {
-        path: "/nolayout",
-        element: <NoLayout />
-    },
-    {
-        path: "/profile",
-        element: <PrivateRoute><HomeProfile /></PrivateRoute>,
-        children: [
-            {
-                index: true,
-                element: <HomeProfile/>
-            },
-        ]
-    },
-    {
-        path: "/profile/order/createorder",
-        element: <CreateOrderPage />
-    },
-    {
-        path: "/profile/order/purchase",
-        element: <PurchasePage />
-    },
-    {
-        path: "/profile/order/technology",
-        element: <TechnologyPage />
-    },
-    {
-        path: "/profile/order/conditions",
-        element: <ConditionsPage />
-    },
-    {
-        path: "/profile/order/contacts",
-        element: <ContactsPage />
-    },
-    {
-        path: "/profile/selection",
-        element: <SelectionPage />
-    },
-    {
-        path: "/VKIDtokenpage",
-        element: <VKIDtokenpage />
-    },
-    {
-        path: "/Yatokenpage",
-        element: <Yatokenpage />
-    }
+  {
+    path: "/",
+    element: <Root />,
+  },
+  {
+    path: "/nolayout",
+    element: <NoLayout />,
+  },
+  {
+    path: "/profile",
+    element: (
+      <PrivateRoute>
+        <HomeProfile />
+      </PrivateRoute>
+    ),
+    children: [
+      {
+        index: true,
+        element: <HomeProfile />,
+      },
+    ],
+  },
+  {
+    path: "/profile/order/createorder",
+    element: <CreateOrderPage />,
+  },
+  {
+    path: "/profile/order/purchase",
+    element: <PurchasePage />,
+  },
+  {
+    path: "/profile/order/technology",
+    element: <TechnologyPage />,
+  },
+  {
+    path: "/profile/order/conditions",
+    element: <ConditionsPage />,
+  },
+  {
+    path: "/profile/order/contacts",
+    element: <ContactsPage />,
+  },
+  {
+    path: "/profile/selection",
+    element: <SelectionPage />,
+  },
+  {
+    path: "/profile/registry",
+    element: <ProcurementRegisterPage />,
+  },
+  {
+    path: "/VKIDtokenpage",
+    element: <VKIDtokenpage />,
+  },
+  {
+    path: "/Yatokenpage",
+    element: <Yatokenpage />,
+  },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-      <Provider store={store}>
-          <RouterProvider router={router} />
-      </Provider>
-            </React.StrictMode>,
-)
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </React.StrictMode>
+);
