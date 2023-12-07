94a0a511fd4060e1fc0734142a5183816e33b4c0
davidpinkevich <davidpinkevich95@gmail.com>
2023-12-08 00:25:05 +0300

    feat: add pag for page

--- src/components/ProcurementRegister/Items/Items.jsx
+++ src/components/ProcurementRegister/Items/Items.jsx
@@ -1,28 +1,41 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import classNames from "classnames/bind";
 import Item from "../Item/Item";
+import PaginationCards from "../PaginationCards/PaginationCards";
 import Loading from "../Loading/Loading";
-import { getAllCards } from "@store/procurementRegister/procRegister.slice";
+import { getCardsByPage } from "../../../utils/helpers/procurementRegister";
+import {
+  getAllCards,
+  changePage,
+} from "@store/procurementRegister/procRegister.slice";
 import PropTypes from "prop-types";
 import styles from "./Items.module.scss";
 
 function Items({ query, open }) {
+  const ref = useRef(null);
   const dispatch = useDispatch();
   const cards = useSelector((state) => state.procRegister.cards);
   const loading = useSelector((state) => state.procRegister.loading);
+  const pageNumber = useSelector((state) => state.procRegister.pageNumber);
 
   useEffect(() => {
     dispatch(getAllCards(query));
-  }, [dispatch, query]);
+    if (cards.length <= 40) dispatch(changePage(1));
+  }, [dispatch, query, cards.length]);
   const classOpen = classNames(styles.cards, styles.cards_open);
   return (
     <>
       {loading && <Loading />}
-      <div className={open ? classOpen : styles.cards}>
+      <div ref={ref} className={open ? classOpen : styles.cards}>
         {!loading &&
-          cards.map((item) => <Item key={item.order_number} {...item} />)}
+          getCardsByPage(cards, pageNumber).map((item) => (
+            <Item key={item.order_number} {...item} />
+          ))}
       </div>
+      {!loading && cards.length > 40 && (
+        <PaginationCards ref={ref} items={cards.length} page={pageNumber} />
+      )}
     </>
   );
 }
