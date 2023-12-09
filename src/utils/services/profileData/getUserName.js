65a2585e7c51b8ec8bd9ae257231530f3d2d1896
davidpinkevich <davidpinkevich95@gmail.com>
2023-12-09 23:22:25 +0300

    fix: clear code

--- src/utils/services/profileData/getUserName.js
+++ src/utils/services/profileData/getUserName.js
@@ -1,19 +1,17 @@
 import axios from "axios";
-import {apiHOST} from "@/utils/constants/apiEndpoints.js";
+import { apiHOST } from "@/utils/constants/apiEndpoints.js";
 
 export default async function getUserName() {
-    return axios
-        .get(`${apiHOST}lk/welcome`, {
-            withCredentials: true
-        })
-        .then((response) => {
-            if (response.data.status === 200) {
-                console.log(response)
-                return response.data.data
-            } else {
-                return false
-            }
-
-        })
-        .catch((error) => console.log(error))
+  return axios
+    .get(`${apiHOST}lk/welcome`, {
+      withCredentials: true,
+    })
+    .then((response) => {
+      if (response.data.status === 200) {
+        return response.data.data;
+      } else {
+        return false;
+      }
+    })
+    .catch((error) => console.log(error));
 }
