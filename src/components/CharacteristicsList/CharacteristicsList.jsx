21eb77e014e43d50f0b3638015ed605984fb3b84
Andrey <teryaev.2004@mail.ru>
2023-12-12 11:30:44 +0300

    Added all characteristics template for application page

--- src/components/CharacteristicsList/CharacteristicsList.jsx
+++ src/components/CharacteristicsList/CharacteristicsList.jsx
@@ -1,7 +1,16 @@
-const CharacteristicsList = () => {
-    return(
+import Characteristic from "../Characteristic/Characteristic";
+import styles from "./CharacteristicsList.module.scss";
 
-    )
-}
+const CharacteristicsList = ({ list }) => {
+  return (
+    <div className={styles.list}>
+      {list.map((el, idx) => {
+        return (
+          <Characteristic key={idx} parameter={el.parameter} value={el.value} />
+        );
+      })}
+    </div>
+  );
+};
 
 export default CharacteristicsList;
