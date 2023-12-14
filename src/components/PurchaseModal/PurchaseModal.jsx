a09eafd554896a24b0611d1eed745271d06eb1c3
lymeYo <lyme1878@mail.ru>
2023-12-14 17:58:56 +0300

    bug fixes

--- src/components/PurchaseModal/PurchaseModal.jsx
+++ src/components/PurchaseModal/PurchaseModal.jsx
@@ -92,7 +92,7 @@ const PurchaseModalWrapper = ({ isOpen, close }) => {
    
    useEffect(() => {
       if (!isOpen || isOpenAccess) return
-      fetch('https://api.legpromrf.ru/subscriptions/create/').then(res => {
+      fetch('https://api.legpromrf.ru/lk/welcome').then(res => {
          const SERVER_DENIED = !res.ok //запрос на сервер
          if (SERVER_DENIED) {
             navigate('/auth')
