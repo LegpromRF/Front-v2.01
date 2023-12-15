9b06b370da136a82afa866e1736db927ad462ce5
lymeYo <lyme1878@mail.ru>
2023-12-15 10:59:01 +0300

    replace registry info

--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -164,23 +164,23 @@ const Home = () => {
                                                 <p className={styles.card__number}>{card.number}</p>
                                                 <div className={styles.card__info}>
                                                     <div className={styles.card__infoItem}>
-                                                        <div className={styles.card__label}>Тираж</div>
+                                                        <div className={styles.card__label}>Количество</div>
                                                         <div
                                                             className={styles.card__description}>{card.circulation}</div>
-                                                    </div>
-                                                    <div className={styles.card__infoItem}>
-                                                        <div className={styles.card__label}>Опубликовано</div>
-                                                        <div
-                                                            className={styles.card__description}>{card.datePublished}</div>
                                                     </div>
                                                     <div className={styles.card__infoItem}>
                                                         <div className={styles.card__label}>Бюджет</div>
                                                         <div className={styles.card__description}>{card.budget} {card.budget || card.budget === 0 ? '₽' : ''}</div>
                                                     </div>
                                                     <div className={styles.card__infoItem}>
-                                                        <div className={styles.card__label}>Сдача</div>
+                                                        <div className={styles.card__label}>Срок поставки</div>
                                                         <div
                                                             className={styles.card__description}>{card.dateChange}</div>
+                                                    </div>
+                                                    <div className={styles.card__infoItem}>
+                                                        <div className={styles.card__label}>Опубликовано</div>
+                                                        <div
+                                                            className={styles.card__description}>{card.datePublished}</div>
                                                     </div>
                                                 </div>
                                             </div>
