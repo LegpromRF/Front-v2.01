d311d6b72ae8ee77e293e42a00a6184a952e6f14
lymeYo <lyme1878@mail.ru>
2023-12-17 16:29:54 +0300

    changed auth checking

--- src/components/PurchaseModal/PurchaseModal.jsx
+++ src/components/PurchaseModal/PurchaseModal.jsx
@@ -2,28 +2,20 @@ import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { apiHOST } from "@/utils/constants/apiEndpoints.js";
 import PurchaseModalContent from './PurchaseModalContent';
-import axios from "axios";
+import Cookies from "js-cookie";
 
 //использую обертку для обработки сценария с неавторизованным пользователем или иным запретом доступа к покупке
 const PurchaseModal = ({ isOpen, close }) => {
-   const [isOpenAccess, setOpenAccess] = useState(false) 
    const navigate = useNavigate()
-   
    useEffect(() => {
-      if (!isOpen || isOpenAccess) return
-      const handleServerReject = () => {
+      const auth = Cookies.get("uuid_user")
+      if (isOpen && !auth) {
          navigate('/auth')
          close()
       }
-      
-      axios.get(`${apiHOST}lk/welcome`, { withCredentials: true }).then(res => {
-         const isServerAccess = res.data.status === 200
-         if (isServerAccess) setOpenAccess(true) 
-         else handleServerReject()
-      }).catch(() => handleServerReject())
-   }, [isOpen, isOpenAccess])
+   }, [isOpen])
    
-   return (isOpenAccess && isOpen) ? <PurchaseModalContent close={close} /> : ''
+   return isOpen ? <PurchaseModalContent close={close} /> : ''
 }
 
 export default PurchaseModal
