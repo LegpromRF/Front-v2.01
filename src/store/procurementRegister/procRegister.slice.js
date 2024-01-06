e079c43f6ebc7b5d0bccd25dc2008e31d0317b45
lymeYo <lyme1878@mail.ru>
2024-01-06 13:22:56 +0300

    bug fixes

--- src/store/procurementRegister/procRegister.slice.js
+++ src/store/procurementRegister/procRegister.slice.js
@@ -9,7 +9,8 @@ export const getAllFilters = createAsyncThunk("users/getFilters", async () => {
 
 export const getAllCards = createAsyncThunk("users/getCards", async (query) => {
   const response = await fetch(`https://api.legpromrf.ru/order_cards?${query}`);
-  return await response.json();
+  const cards = await response.json() 
+  return cards || [];
 });
 
 export const procRegisterSlice = createSlice({
