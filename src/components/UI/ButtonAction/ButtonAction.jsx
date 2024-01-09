6e34894c85ae4b7215f12afbadd43d0320d8d354
Andrey <teryaev.2004@mail.ru>
2024-01-09 12:58:34 +0300

    Some fixes

--- src/components/UI/ButtonAction/ButtonAction.jsx
+++ src/components/UI/ButtonAction/ButtonAction.jsx
@@ -1,8 +1,8 @@
 import styles from "./ButtonAction.module.scss";
 
-const ButtonAction = ({ title, icon }) => {
+const ButtonAction = ({ title, icon, onClick }) => {
   return (
-    <button className={styles.wrapper}>
+    <button className={styles.wrapper} onClick={onClick}>
       <div className={styles.titleBlock}>{title}</div>
       <div className={styles.iconWrapper}>{icon}</div>
     </button>
